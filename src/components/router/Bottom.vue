cdee5634fd18bc6bd5e0026e2d9b48b4894e880a
zhangxiandi <166609304+zhangxiandi@users.noreply.github.com>
2025-05-24 13:54:50 +0800

    Add files via upload

---
+++ src/components/router/Bottom.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="page-bottom">
+        <p>
+            ~~~~~~~~~~~~~~~~~~~~~~
+        </p>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+.page-bottom {
+    margin-top: 1em;
+    background-color: Ivory;
+}
+.page-bottom p {
+    color: bisque;
+}
+</style>
---
+++ src/components/router/Siege.vue
@@ -0,0 +1,18 @@
+<template>
+<Synopsis :init="index"></Synopsis>
+<Collapse :init="index"></Collapse>
+<Resource></Resource>
+<Bottom></Bottom>
+</template>
+<script setup>
+import Synopsis from './Synopsis.vue'
+import Collapse from './siege/Collapse.vue'
+import Resource from './siege/Resource.vue';
+import Bottom from './Bottom.vue';
+const props = defineProps({id: Number})
+const index = Number(props.id);
+
+</script>
+<style>
+
+</style>
